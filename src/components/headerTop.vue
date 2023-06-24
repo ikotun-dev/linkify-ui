4b82cc0673824a1ffe013879e636d1bbd57660cb
Ikotun Collins <danlogan2003@gmail.com>
2023-06-24 20:13:59 +0100

    image

--- src/components/headerTop.vue
+++ src/components/headerTop.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>
-  <div class="bg-black lg:h-20 sm:h-16 pl-36">
+  <div class="h-screen">
+  <div class="bg-green-950 lg:h-20 sm:h-16 pl-36">
     <h3 class="font-pop font-bold text-3xl text-white lg:pt-6 sm:pb-4 sm:pt-4">Linkify</h3>
   </div>
-  <div class="bg-white h-120  lg:flex-row sm:flex-col sm:pt-36 lg:pt-12 sm:flex lg:justify-center lg:items-center">
-    
-    <div>
-      <h2 class="lg:text-6xl sm:text-3xl font-pop lg:pl-20 sm:pl-12">Enter original url :<br><h4 class="lg:pl-20 sm:pl-12 font-pop ">convert long urls <br> to tinyurls quickly, for easy sharing.</h4></h2>
+  
+  <div class="lg:flex lg:items-center">
+    <div class="lg:mt-16 lg:ml-32 sm:mt-8 sm:ml-4">
+      <img class="sm:h-96 lg:h-120" :src="require('../assets/3918929.jpg')"/>
     </div>
-    <div class="mb-12 ml-4">
-      <input class="lg:w-96 lg:h-16 focus:font-pop focus:text-xl px-4 py-2 rounded-md focus:outline-none border-2 border-black"  type="text"/>
+
+    <div>
+      <h2 class="font-pop font-bold lg:text-6xl sm:text-3xl lg:ml-8 text-green-950 sm:ml-6">Enter the original url</h2>
+      <h4 class="ml-8 font-pop lg:text-xl sm:text-md font-bold sm:ml-6">convert long urls to tiny urls for easy sharing</h4>
     </div>
   </div>
 
-  <div class=" bg-black buttom pb-0 mb-0 h-16">
-  </div>
   </div>
 </template>
