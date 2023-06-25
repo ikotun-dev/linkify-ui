281ebc0c3b54346ed26b0743ce70dda5c07339be
Ikotun Collins <danlogan2003@gmail.com>
2023-06-25 14:13:21 +0100

    fixes

--- src/components/headerTop.vue
+++ src/components/headerTop.vue
@@ -9,7 +9,7 @@
       <img class="sm:h-72 lg:h-100" :src="require('../assets/3918929.jpg')"/>
     </div>
 
-    <div>
+    <div class="sm:ml-8 sm:mt-8">
       <h2 class="font-pop font-bold lg:text-6xl sm:text-3xl lg:ml-8 text-green-950 sm:ml-6">Enter the original url</h2>
       <h4 class="lg:ml-8 font-pop lg:text-xl lg:mt-2 sm:text-md font-bold sm:ml-6 sm:mt-2">convert long urls to tiny urls for easy sharing</h4>
 
@@ -20,9 +20,9 @@
         </div>
       </div>
 
-      <div v-show="short_url" class="flex sm:w-67 lg:w-78 border-2 border-green-800 lg:ml-8 lg:mt-2 sm:ml-6 sm:mt-4">
-        <input class="lg:w-76 sm:w-64 focus:outline-none py-2 px-2" type="text"/>
-        <div class="pt-2 pl-4 bg-green-800 w-12 text-xl text-white font-bold hover:text-white">
+      <div v-if="short_url" class="flex sm:w-67 lg:w-78 border-2 border-green-800 lg:ml-8 lg:mt-2 sm:ml-6 sm:mt-4">
+        <input v-model="url" class="lg:w-76 sm:w-64 focus:outline-none py-2 px-2" type="text"/>
+        <div @click="copyUrl" class="pt-2 pl-4 bg-green-800 w-12 text-xl text-white font-bold hover:text-white">
         <i class="fas fa-copy"></i>
         </div>
       </div>
@@ -43,6 +43,7 @@ export default{
   data() { 
     return {
       short_url : false,
+      url :"",
       long_url : "",
     };
   },
@@ -54,9 +55,24 @@ export default{
 
         if (res.status == 200){
           console.log("login was successful");
-          console.log(res.data.short_url)
+          this.url = res.data.short_url
+          console.log(this.url)
+          this.short_url =  true
+        }
+        else {
+          console.log(res.errors)
         }
     },
+    copyUrl() {
+  navigator.clipboard.writeText(this.url)
+    .then(() => {
+      console.log('URL copied to clipboard');
+    })
+    .catch((error) => {
+      console.error('Failed to copy URL: ', error);
+    });
+}
+
   },
 
 }
